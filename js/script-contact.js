81e87d07524acfac81389790b41b18ba3c9145b5
Leonardo van Dahl <111518533+Leonardo190392@users.noreply.github.com>
2022-12-01 13:20:02 +0100

    test

--- js/script-contact.js
+++ js/script-contact.js
@@ -1,4 +1,29 @@
+//Header and sidebar
+async function includeHTML() {
+    let includeElements = document.querySelectorAll('[w3-include-html]');
+    for (let i = 0; i < includeElements.length; i++) {
+        const element = includeElements[i];
+        file = element.getAttribute("w3-include-html"); // "includes/header.html"
+        let resp = await fetch(file);
+        if (resp.ok) {
+            element.innerHTML = await resp.text();
+        } else {
+            element.innerHTML = 'Page not found';
+        }
+    }
+}
+
+
+
+
+
+
+
+/*
 let contacts = ['Anne Fischer', 'Bert Blödel', 'Claudine Kauf', 'Dirk Dach', 'Anton Ameise', 'Babette Bügel', 'Eugen Esel', 'Felicitas Fuchs'];
+
+let email = ['']
+
 
 var map = contacts.reduce((p, c) => {
     let char = c.charAt(0).toUpperCase();
@@ -8,11 +33,13 @@ var map = contacts.reduce((p, c) => {
   
   var result = Object.keys(map).map(k => ({
     letter: k,
-    names: map[k]
+    names: map[k],
+    email: map[k]
   }));
 
 
 function render(){
+    
     generateLetters();
     
     console.log(result);
@@ -22,8 +49,9 @@ function generateLetters(){
     for (let i = 0; i < result.length; i++) {
         const letter = result[i];
         
-        document.getElementById('contactlist').innerHTML += /*html*/ ` 
+        document.getElementById('contactlist').innerHTML += /*html*//* ` 
             <div class="letters">${letter['letter']}</div>
+            
             <div id='letters${i}'></div>
             `;
 
@@ -36,12 +64,12 @@ function generateContacts(i){
         const contact =  result[i]['names'][j];
         console.log(contact)
 
-        document.getElementById(`letters${i}`).innerHTML += /*html*/ `
+        document.getElementById(`letters${i}`).innerHTML += /*html*//* `
         <div id='singleContact${j}'>${contact}</div>
         `;
     }
 }
 
-
+*/
 
 
