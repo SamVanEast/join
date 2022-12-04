2d86a0a88653cda0d63e7d36da7d5a9381c6244d
Creativity12345 <dachrinnen.wohnwagen_0k@icloud.com>
2022-12-04 12:15:12 +0100

    dueDate works (but without 0 on day)

--- js/add_task.js
+++ js/add_task.js
@@ -2,6 +2,15 @@ function myFunction() {
     console.log('Ready!');
 }
 
+
+function setDate() {
+    var date = new Date();
+    const formatDate = (date) => { let formatted_date = date.getDate() + "/" + (date.getMonth() + 1) + "/" + date.getFullYear(); return formatted_date; }
+    console.log(formatDate(date));
+    document.getElementById('dueDate').value = formatDate(date);
+}
+
+
 function clearFields() {
     document.getElementById('form').reset();
 }
