861004f8a54a60aebadcd11d8e6ca4917cd5f27a
unknown <t-wagner@web.de>
2022-12-05 19:38:05 +0100

    board doesnt work

--- js/board.js
+++ js/board.js
@@ -1,18 +1,3 @@
-//Header and sidebar
-async function includeHTML() {
-    let includeElements = document.querySelectorAll('[w3-include-html]');
-    for (let i = 0; i < includeElements.length; i++) {
-        const element = includeElements[i];
-        file = element.getAttribute("w3-include-html"); // "includes/header.html"
-        let resp = await fetch(file);
-        if (resp.ok) {
-            element.innerHTML = await resp.text();
-        } else {
-            element.innerHTML = 'Page not found';
-        }
-    }
-}
-
 let tasks = [{
     "id": 0,
     "status": "todo",
@@ -50,7 +35,6 @@ let tasks = [{
 let currentDraggedElement;
 
 function init() {
-    includeHTML();
     filterTodo();
     filterProgress();
     filterFeedback();
@@ -146,6 +130,7 @@ function checkBgColor(element) {
 /**
  * 
  */
+
 function closeOpenTask() {
     document.getElementById('openTask').classList.add('d-none');
 
