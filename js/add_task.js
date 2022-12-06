ba2c9486b55e0ce5025ffaca08b80fca855c723a
Creativity12345 <dachrinnen.wohnwagen_0k@icloud.com>
2022-12-06 19:59:58 +0100

    dropdowns changed (dont work)

--- js/add_task.js
+++ js/add_task.js
@@ -3,6 +3,11 @@ function myFunction() {
 }
 
 
+function renderCategorys(){
+    document.getElementById('dropdownCategory').classList.add('showAllCategorys');
+}
+
+
 function setDate() {
     var date = new Date();
     const formatDate = (date) => { let formatted_date = (date.getDate() < 10 ? '0' : '') + date.getDate() + "/" + (date.getMonth() + 1) + "/" + date.getFullYear(); return formatted_date; }
