9f259275d1568432319aa98da03c03ed8922dada
Samuel Haas <Samuel03102001@gmail.com>
2022-12-07 10:51:03 +0100

    update side_bar.js and started with mobile version

--- js/script-contact.js
+++ js/script-contact.js
@@ -23,26 +23,8 @@ let contacts = [
 ]
 
 function render(){
-    includeHTML();
     generateContactlist();
 }
-
-//Header and sidebar
-async function includeHTML() {
-    let includeElements = document.querySelectorAll('[w3-include-html]');
-    for (let i = 0; i < includeElements.length; i++) {
-        const element = includeElements[i];
-        file = element.getAttribute("w3-include-html"); // "includes/header.html"
-        let resp = await fetch(file);
-        if (resp.ok) {
-            element.innerHTML = await resp.text();
-        } else {
-            element.innerHTML = 'Page not found';
-        }
-    }
-}
-
-
 
 function generateContactlist() {
   letters = [];
--- js/side_bar.js
+++ js/side_bar.js
@@ -1,3 +1,6 @@
+/**
+ * Sucht nach dem Attribute w3-include-html um darin ein HtMl-template rein zu laden
+ */
 async function includeHTML() {
     let includeElements = document.querySelectorAll('[w3-include-html]');
     for (let i = 0; i < includeElements.length; i++) {
@@ -11,25 +14,56 @@ async function includeHTML() {
         }
     }
 }
-
-function loadContent(wichContent){//Den angeklickten Inhalt laden
+/**
+ * Je nachdem auf welchem Reiter man klickt wird das entsprechende template geladen
+ * ebenso wird dem angeklickten Reiter eine andere Background-color gegeben
+ * @param {string} wichContent gibt an welches HTML-template geladen werden muss, indem es ein Teil des Pfads ist
+ */
+async function loadContent(wichContent) {//Den angeklickten Inhalt laden
     let content = document.getElementById('content');
     let tab = document.getElementById(`tab-${wichContent}`);
+    removeDarkBlue();
     if (wichContent == 'help') {
         document.getElementById('qoestion-mark-img').classList.add('d-none');
-    }else{
+    } else {
         document.getElementById('qoestion-mark-img').classList.remove('d-none');
         tab.classList.add('darkBlue');
     }
     content.innerHTML = `<div w3-include-html="${wichContent}.html"></div>`;
-    removeDarkBlue();
-    includeHTML();
+    await includeHTML();
+    if (wichContent== 'board') {
+        init();
+    }
+    if (wichContent== 'contacts') {
+        render();
+    }
 }
-
-function removeDarkBlue(){//Überrall den dunkleren Farbton löschen.
+/**
+ * Es löscht von allen Reitern die diese Background-color haben könnten, diese.
+ */
+function removeDarkBlue() {//Überrall den dunkleren Farbton löschen.
     document.getElementById('tab-summary').classList.remove('darkBlue');
     document.getElementById('tab-board').classList.remove('darkBlue');
     document.getElementById('tab-add_task').classList.remove('darkBlue');
     document.getElementById('tab-contacts').classList.remove('darkBlue');
     document.getElementById('tab-legal_notice').classList.remove('darkBlue');
+}
+/**
+ * entfernt dem log out button die class d-none 
+ */
+function showLogOutButton() {
+    document.getElementById('log-out').classList.remove('d-none');
+}
+/**
+ * fügt dem log out buttton die Class d-none hinzu
+ */
+function closeLogOutButton(){
+    document.getElementById('log-out').classList.add('d-none');
+}
+/**
+ * sorgt dafür das diese div aufwelche geklickt wird nicht von einer anderen function beeinflusst wird
+ * @param {*} event 
+ */
+function save(event) {
+    event.stopPropagation();
 }
