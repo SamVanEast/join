a92bcec00ea1b59e6031b4846e91d68592e0ddb1
Thomas Wagner <t-wagner@web.de>
2022-12-08 11:17:36 +0100

    trying to include add_tasks

--- js/board.js
+++ js/board.js
@@ -1,20 +1,6 @@
 let currentDraggedElement;
 
 
-async function includeHTML() {
-    let includeElements = document.querySelectorAll('[w3-include-html]');
-    for (let i = 0; i < includeElements.length; i++) {
-        const element = includeElements[i];
-        file = element.getAttribute("w3-include-html"); // "includes/header.html"
-        let resp = await fetch(file);
-        if (resp.ok) {
-            element.innerHTML = await resp.text();
-        } else {
-            element.innerHTML = 'Page not found';
-        }
-    }
-}
-
 
 async function init() {
     await getTasks();
@@ -147,3 +133,7 @@ function removeHighlight(id) {
     document.getElementById(id).classList.remove('dragAreaHighlight');
 }
 
+function addTask() {
+    document.getElementById('addNewTask').classList.remove('d-none');
+    
+}
