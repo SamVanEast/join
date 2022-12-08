cc8b49811091f7c6060a8a440c4469fa13134d33
Thomas Wagner <t-wagner@web.de>
2022-12-08 10:53:15 +0100

    working on the board

--- js/board.js
+++ js/board.js
@@ -1,5 +1,21 @@
 let currentDraggedElement;
 
+
+async function includeHTML() {
+    let includeElements = document.querySelectorAll('[w3-include-html]');
+    for (let i = 0; i < includeElements.length; i++) {
+        const element = includeElements[i];
+        file = element.getAttribute("w3-include-html"); // "includes/header.html"
+        let resp = await fetch(file);
+        if (resp.ok) {
+            element.innerHTML = await resp.text();
+        } else {
+            element.innerHTML = 'Page not found';
+        }
+    }
+}
+
+
 async function init() {
     await getTasks();
     
@@ -14,7 +30,7 @@ async function getTasks() {
     filterProgress(tasks);
     filterFeedback(tasks);
     filterDone(tasks);
-
+    
 }
 
 function filterTodo(tasks) {
