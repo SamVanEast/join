315908f0fd91d782373cd4e1cea18f11d9e8626c
Leonardo van Dahl <111518533+Leonardo190392@users.noreply.github.com>
2022-12-08 11:12:31 +0100

    add screens to login.html

--- js/login.js
+++ js/login.js
@@ -1,4 +1,17 @@
 
 function openSignUp(){
     document.getElementById('signUp').classList.remove('d-none');
+    document.getElementById('login').classList.add('d-none');
+}
+
+function backToLogin(){
+    document.getElementById('signUp').classList.add('d-none');
+    document.getElementById('forgot').classList.add('d-none');
+    document.getElementById('reset').classList.add('d-none');
+    document.getElementById('login').classList.remove('d-none');
+}
+
+function showForgotScreen() {
+    document.getElementById('forgot').classList.remove('d-none');
+    document.getElementById('login').classList.add('d-none');
 }
