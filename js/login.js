a95c14510bf5ebcb2d0f77fdf2e8bc202bb9affb
Leonardo van Dahl <111518533+Leonardo190392@users.noreply.github.com>
2022-12-08 15:09:59 +0100

    update login

--- js/login.js
+++ js/login.js
@@ -1,3 +1,11 @@
+let user = [
+    {
+        "name":"leo",
+        "email":"leo@gmx.de",
+        "password":"nardo"
+    }
+]
+
 
 function openSignUp(){
     document.getElementById('signUp').classList.remove('d-none');
@@ -14,4 +22,22 @@ function backToLogin(){
 function showForgotScreen() {
     document.getElementById('forgot').classList.remove('d-none');
     document.getElementById('login').classList.add('d-none');
+}
+
+function checkLogin() {
+    let email = user[0]['email'];
+    let password = user[0]['password'];
+    let inputPassword = document.getElementById('loginPassword').value;
+    let inputEmail = document.getElementById('loginEmail').value;
+    console.log(email, password);
+    console.log(inputPassword, inputEmail);
+
+    if(email === inputEmail && password === inputPassword){
+        console.log('LOGIN');
+        location.replace('/assets/templates/side_bar.html');
+    }
+    else {
+        console.log('WRONG!');
+    }
+    
 }
