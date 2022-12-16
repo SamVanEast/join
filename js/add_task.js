c01fddd788d8f347520e187c2e0c1ddbbb96d2f1
Creativity12345 <dachrinnen.wohnwagen_0k@icloud.com>
2022-12-16 13:23:24 +0100

    add categorys

--- js/add_task.js
+++ js/add_task.js
@@ -5,6 +5,7 @@
  */
 
 let contact;
+let categorys = ['Media', 'Backoffice', 'Marketing', 'Design'];
 
 
 /**
@@ -212,7 +213,7 @@ function submitTask() {
         'headline': headline,
         'desc': desc,
         'status': 'todo',
-        // 'category': cat,
+        'category': 'Media',
         // 'assignedTo': assigned,
         // 'dueDate': dueDate,
         // 'prio': prio,
