691818dec7cb8fd52d8fc6c8b94ce599dd5fe089
Thomas Wagner <t-wagner@web.de>
2023-01-06 17:58:45 +0100

    board more than 4 users

--- js/board.js
+++ js/board.js
@@ -505,10 +505,19 @@ function filterBoardDone() {
 
 function redernTask(element) {
     startDragging(element.id);
+
+    let userCount = 0;
+
     for (let j = 0; j < element.assignedTo.length; j++) {
         const assigned = element.assignedTo[j];
         const bgcolor = element.bgcolor[j];
-        document.getElementById(`people${element.id}`).innerHTML += getPeopleHTML(assigned, bgcolor);
+        userCount++;
+        if (userCount < 4) {
+            document.getElementById(`people${element.id}`).innerHTML += getPeopleHTML(assigned, bgcolor);
+        } else {
+            document.getElementById(`people${element.id}`).innerHTML = `${userCount - 3}+`;
+            break;
+        }
     }
     checkProgressbar();
 }
